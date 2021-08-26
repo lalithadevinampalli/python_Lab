7996fd60c34cbb09f62ee7fa09515e67a7ab0577
lalithadevinampalli <87235282+lalithadevinampalli@users.noreply.github.com>
2021-08-26 09:38:13 +0530

    Add files via upload

---
+++ multiply_matrix.py
@@ -0,0 +1,25 @@
+X = [[1,2,3],  
+       [4,5,6],  
+       [7,8,9]]  
+  
+Y = [[0,1,2],  
+      [3,4,5],  
+      [6,7,8]]
+result=[[0,0,0],
+        [0,0,0],
+        [0,0,0]]
+    
+  
+# iterate through rows of X  
+for i in range(len(X)):  
+   for j in range(len(Y[0])):  
+       for k in range(len(Y)):  
+           result[i][j] += X[i][k] * Y[k][j]
+
+
+for r in result:  
+   print(r)
+   
+
+
+
