1f7f258b95e642ffcdbcce8a21acf5d298a962bc
lalithadevinampalli <87235282+lalithadevinampalli@users.noreply.github.com>
2021-09-22 15:06:45 +0530

    Delete substring.py

--- substring.py
+++
@@ -1,9 +0,0 @@
-def check(str1, sstr): 
-   if (str1.find(sstr) == -1): 
-      print(sstr,"IS NOT PRESENT IN THE GIVEN STRING") 
-   else: 
-      print(sstr,"IS PRESENT IN THE GIVEN STRING") 
-# Driver code 
-str1 = input("Enter the string ::>")
-sstr=input("Enter Substring ::>")
-check(str1, sstr) 
